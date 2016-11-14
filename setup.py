b19580c745fdf3fd0751d98a253260fc582a7062
Pratima <chettripratima@gmail.com>
2016-11-14 16:20:39 +0545

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+		name = 'assignment' , 
+		version = '0.1' , 
+		description = 'calculation of minimum price of given book' ,
+		url = 'https://github.com/ptyma/MinimumPriceBookScraper' ,
+		author = 'Pratima Chettri' , 
+		author_email = 'chettripratima@gmail.com' , 
+		packages = ['assignment'] 
+	)
+		
+		
